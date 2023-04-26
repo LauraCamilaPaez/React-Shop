63e897e5d07d185aaaf9850cc10be31b758ce57f
LauraCamilaPaez <2104laura2104@gmail.com>
2023-04-26 09:06:53 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./routes/App";
+
+createRoot(document.getElementById('app')).render(<App />);
+
+// ReactDOM.render(<App />, document.getElementById("app"));
